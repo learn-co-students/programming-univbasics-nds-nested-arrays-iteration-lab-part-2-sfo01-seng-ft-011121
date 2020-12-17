5ed3395760bbe84cbf2429fb9dc9468cc884dbf3
Erik Huang <erikhuangdrums@gmail.com>
2020-12-17 16:45:33 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,9 @@
-def find_min_in_nested_arrays(src)
-  min_arr = []
-  src.each do |sb|
-    min = sb[0]
-    sb.each { |n| min = n if n < min }
-    min_arr << min
+def find_min_in_nested_arrays(arr)
+  output_arr = []
+  arr.each do |sub_arr|
+    min = Float::INFINITY
+    sub_arr.each { |num| min = min < num ? min : num }
+    output_arr << min
   end
-  min_arr
+  output_arr
 end
